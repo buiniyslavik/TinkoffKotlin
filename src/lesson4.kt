9274ab6844cf1bca8ed9c18a8bed9f10dd5483a0
svosin <svosin@airmail.cc>
2019-10-06 12:08:49 +0300

    crappy game

--- src/lesson4.kt
+++ src/lesson4.kt
@@ -1,10 +1,9 @@
+import java.lang.Exception
 import kotlin.system.exitProcess
 
 abstract class Pet() {
     abstract val name: String
     abstract val age: Int
-    var howDirty = 0
-    var howHungry = 0
 //    abstract fun feed()
 //    abstract fun clean()
 }
@@ -88,22 +87,89 @@ class Fish(name: String, age: Int) : Pet(), Swimable
 
 fun main() {
     println("Choose mode:\n1. Play\n2. Demo")
-    when(readLine()?.toInt())
-    {
-        1 -> play()
-        2 -> demo()
-        else ->
-        {
-            println("Wrong option selected!")
-            exitProcess(-1)
+    try {
+        when (readLine()?.toInt()) {
+            1 -> play()
+            2 -> demo()
+            else -> {
+                println("Wrong option selected!")
+                exitProcess(-1)
+            }
         }
+    }
+    catch(e: Exception)
+    {
+        println("you're doing it wrong")
+        exitProcess(-1)
     }
 }
 
 fun play() {
-    println("Choose your destiny:")
-    println("1. Собака")
-    println("2. Кот")
+    class playablePet(name: String, age: Int): Pet()
+    {
+        override val name = name
+        override val age = age
+        var howHungry = 1
+        var howDirty = 1
+        fun feed()
+        {
+            howHungry = 0
+        }
+        fun clean()
+        {
+            howDirty = 0
+        }
+        fun newDay()
+        {
+            howHungry++
+            howDirty++
+        }
+        fun hasEscaped(): Boolean {
+            return (howDirty>1)
+        }
+    }
+
+    println("Enter your pet's name:")
+    val name: String
+    val age: Int
+    val tempName = readLine()
+    println("Enter your pet's age:")
+    val tempAge = readLine()
+    try {
+        name = if (tempName != null) tempName else throw NullPointerException("Enter something dammit")
+        age =
+            (if (tempAge != null) tempAge else throw NullPointerException("Enter something dammit")).toInt()
+    }
+    catch(e: Exception) {
+        println("you're doing it wrong")
+        exitProcess(-1)
+    }
+
+    val myPet = playablePet(name, age)
+    var dayCount = 1
+    while(true)
+    {
+        println("Day $dayCount; ${myPet.name} is ${myPet.age} years old; hunger ${myPet.howHungry} dirt ${myPet.howDirty}.\n" +
+                "Whatcha doin' today?\n" +
+                "1. Feed\n" +
+                "2. Clean\n" +
+                "3. Nothing\n")
+        when(readLine()?.toInt())
+        {
+            1 -> myPet.feed()
+            2 -> myPet.clean()
+            3 -> println("Doing nothing")
+            else -> println("Bad input, doing nothing")
+        }
+        if(myPet.hasEscaped())
+        {
+            println("Your pet has escaped!\nGame over")
+            exitProcess(0)
+        }
+        myPet.newDay()
+        dayCount++
+
+    }
 }
 
 fun demo() {
