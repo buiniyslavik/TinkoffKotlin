54b2215a852a69181a8f1c4e01d87128eb5132a5
svosin <svosin@airmail.cc>
2019-10-08 18:41:57 +0300

    comparators

--- src/persons.kt
+++ src/persons.kt
@@ -4,9 +4,43 @@ data class Person(
     val age: Int,
     val zipCode: String?,
     val phoneNumber: String?
-)
+) {
+    companion object {
+        fun compare(a: Person, b: Person): Int {
+            val deltaFirstName = a.lastName.compareTo(b.lastName)
+            if (deltaFirstName != 0) return deltaFirstName
+            else {
+                val deltaLastName = a.lastName.compareTo(b.lastName)
+                if (deltaLastName != 0) return deltaLastName
+                else {
+                    val deltaAge = a.age.compareTo(b.age)
+                    if (deltaAge != 0) return deltaAge
+                    else return 0
+                }
+                // sorry for making you read this
+            }
+        }
+    }
+}
 
-fun main(array: Array<String>) {
+object PersCom : Comparator<Person> {
+    override fun compare(a: Person, b: Person): Int {
+        val deltaFirstName = a.lastName.compareTo(b.lastName)
+        if (deltaFirstName != 0) return deltaFirstName
+        else {
+            val deltaLastName = a.lastName.compareTo(b.lastName)
+            if (deltaLastName != 0) return deltaLastName
+            else {
+                val deltaAge = a.age.compareTo(b.age)
+                if (deltaAge != 0) return deltaAge
+                else return 0
+            }
+            // sorry for making you read this
+        }
+    }
+}
+
+fun main(args: Array<String>) {
     val vasya = Person("Vasily", "Pupkin", 40, "12345", "+12345678901")
     val vasyasClone = vasya.copy()
     val chicken = Person("Kudah", "Tah-Tah", 1, null, null)
@@ -18,4 +52,11 @@ fun main(array: Array<String>) {
     } else {
         println("vasya не идентичен курице")
     }
+    if (Person.compare(vasya, vasyasClone) == 0) {
+        println("vasya идентичен vasyasClone, теперь и через companion object")
+    }
+    if (PersCom.compare(vasya, vasyasClone) == 0) {
+        println("vasya идентичен vasyasClone, теперь и через object")
+    }
+
 }
