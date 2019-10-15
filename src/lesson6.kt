ca2461eb84eb5f45e7c86956ca88f7a1d2a3762a
svosin <svosin@airmail.cc>
2019-10-15 19:36:16 +0300

    extensions

--- src/lesson6.kt
+++ src/lesson6.kt
@@ -4,6 +4,13 @@ data class Animal(
     var sex: Boolean? = null
 )
 
+fun Animal.isAdult(): Boolean {
+    return age >= 2
+}
+
+val Animal.respectableNickname: String
+    get() = "Dear $nickname"
+
 fun main(args: Array<String>) {
     val dog = Animal()
     with(dog) {
@@ -12,12 +19,16 @@ fun main(args: Array<String>) {
         this.sex = true
     }
     println(dog.toString())
+    println(dog.isAdult())
+    println(dog.respectableNickname)
     val parrot = Animal().apply {
         this.nickname = "Kesha"
         this.age = 1
         this.sex = true
     }
     println(parrot.toString())
+    println(parrot.isAdult())
+    println(parrot.respectableNickname)
     val cat = Animal()
     cat.let {
         it.nickname = "Mashka"
@@ -25,10 +36,14 @@ fun main(args: Array<String>) {
         it.sex = false
     }
     println(cat.toString())
+    println(cat.isAdult())
+    println(cat.respectableNickname)
     val DomesticAmoeba = Animal().also {
         it.nickname = "John"
         it.age = 0
         it.sex = null
     }
     println(DomesticAmoeba.toString())
+    println(DomesticAmoeba.isAdult())
+    println(DomesticAmoeba.respectableNickname)
 }
