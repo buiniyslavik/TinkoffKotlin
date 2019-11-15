5453bf23b5bfa184694cfdf5ea3ad19d6ec6ad85
svosin <svosin@airmail.cc>
2019-11-15 10:37:40 +0300

    initial commit

--- src/filterColl.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val names = arrayListOf<String>("Хрюша", "Степаша", "Филя", "Гуля")
-    val filtered = names.filter {
-        it.length < 6
-    }
-    filtered.forEach { println(it) }
-}
