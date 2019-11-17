43524fae736e2926734e56e53aa07aa172761cec
svosin <svosin@airmail.cc>
2019-11-17 20:58:05 +0300

    semi-working version

--- src/dsl.kt
+++ src/dsl.kt
@@ -1,3 +1,34 @@
-fun main() {
+class objContext {
+    infix fun name(n: String) {
+        println("<name>$n</name>")
+    }
 
+    infix fun surname(n: String) {
+        println("<surname>$n</surname>")
+    }
+
+    infix fun birthDate(d: String) {
+        println("<birthDate>$d</birthDate>")
+    }
+
+    infix fun addresses(a: List<String>) {
+        println("<addresses>")
+        a.forEach { println("<address>$it</address>") }
+        println("</addresses>")
+    }
+}
+
+fun obj(block: (objContext) -> Unit) {
+    println("<object>")
+    block(objContext())
+    println("</object>")
+}
+
+fun main() {
+    val dude = obj {
+        it name "vasya"
+        it surname "pupkin"
+        it birthDate "01.01.1980"
+        it addresses listOf("addr1", "addr2")
+    }
 }
